28f3af6a0f2de3fd15b4192f8784ab7ad75f76e4
Andre Machado <andre.machado@my.istec.pt>
2017-05-22 21:17:37 +0100

    Initial Commit

---
+++ RGBA-45068/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBA-45068
+//
+//  Created by Andre Machado on 22/05/2017.
+//  Copyright © 2017 Andre Machado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
